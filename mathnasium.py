28db08bddda038b6edb7d168a9eb468d373238aa
coreylazenby22 <coreylazenby22@gmail.com>
2024-08-06 10:15:17 -0600

    Learning Arrays!

--- mathnasium.py
+++ mathnasium.py
@@ -7,17 +7,27 @@ array1 = np.array([1, 3, 5])
 array2 = np.array([2, 4, 6])
 
 
-def arrayDotProduct(arr1: List[int], arr2: List[int]) -> int:
+
+
+class NeuralMath:
     
-    dot_product = np.dot(arr1, arr2)
+    def arrayDotProduct(self, arr1: List[int], arr2: List[int]) -> int:
+        
+        dot_product = np.dot(arr1, arr2)
 
 
-    print(f"\nArray 1: {arr1}\nArray 2: {arr2}")
+        print(f"\nArray 1: {arr1}\nArray 2: {arr2}")
 
-    print(f"Dot Product: {dot_product}")
+        print(f"Dot Product: {dot_product}")
 
     
-    return dot_product
+        return dot_product
 
-arrayDotProduct(array1, array2)
+
+
+
+NeuralMath().arrayDotProduct(array1, array2)
+
+
+
 
