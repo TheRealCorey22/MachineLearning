5e182bb933fa7bb198b6cd72f1e2dfb78f5ee0b5
coreylazenby22 <coreylazenby22@gmail.com>
2024-08-06 10:15:27 -0600

    Learning Arrays!

--- mathnasium.py
+++ mathnasium.py
@@ -7,27 +7,34 @@ array1 = np.array([1, 3, 5])
 array2 = np.array([2, 4, 6])
 
 
-
-
 class NeuralMath:
     
-    def arrayDotProduct(self, arr1: List[int], arr2: List[int]) -> int:
+    def arrayOperations(self, arr1: List[int], arr2: List[int]) -> int:
         
+
+        maximum = np.maximum(arr1, arr2)
+
+
         dot_product = np.dot(arr1, arr2)
 
 
         print(f"\nArray 1: {arr1}\nArray 2: {arr2}")
 
-        print(f"Dot Product: {dot_product}")
+        print(f"\nDot Product: {dot_product}")
+
+        print(f"\nMaximum: {maximum}")
 
     
-        return dot_product
+        return dot_product, maximum
+
+
+
+        
+    
 
 
 
 
-NeuralMath().arrayDotProduct(array1, array2)
 
 
-
-
+NeuralMath().arrayOperations(array1, array2)
